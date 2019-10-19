588397a94aaf445daf22b38d9e933e5da6acc06f
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-19 23:04:11 +0700

    4_3 Modify 1

--- Exercise-DemeritPointsCalculator4_3/Fundamentals/DemeritPointsCalculator.cs
+++ Exercise-DemeritPointsCalculator4_3/Fundamentals/DemeritPointsCalculator.cs
@@ -9,11 +9,10 @@ namespace Exercise_DemeritPointsCalculator4_3.Fundamentals
     public class DemeritPointsCalculator
     {
         private const int SpeedLimit = 65;
-        private const int MaxSpeed = 300;
-
+        
         public int CalculateDemeritPoints(int speed)
         {
-            if (speed < 0 || speed > MaxSpeed)
+            if (speed < 0)
                 throw new ArgumentOutOfRangeException();
 
             if (speed <= SpeedLimit) return 0;
